13bd42e437ad87ff1e31666f331c86c608f45187
S. M. Redwan <smredwan.info@gmail.com>
2024-12-24 23:49:38 +0600

    error invoiceservices constructor

--- src/app/Container.php
+++ src/app/Container.php
@@ -36,29 +36,48 @@ class Container Implements ContainerInterface
     {
         $reflectionClass = new \ReflectionClass($id);
 
-        if (! $reflectionClass->isInstantiable()) {
+        if (!$reflectionClass->isInstantiable()) {
             throw new ContainerException('Class "' . $id . '" is not instantiable');
         }
 
         $constructor = $reflectionClass->getConstructor();
 
-        if(! $constructor) {
+        if (!$constructor) {
             return new $id;
         }
 
         $parameters = $constructor->getParameters();
 
-        if(! $parameters) {
+        if (!$parameters) {
             return new $id;
         }
 
-        $dependencies = array_map(function(\ReflectionParameter $param) {
+        $dependencies = array_map(function (\ReflectionParameter $param) use ($id) {
             $name = $param->getName();
             $type = $param->getType();
 
-            if (! $type) {
+            if (!$type) {
                 throw new ContainerException('Parameter "' . $name . '" of type "' . $name . '" is required');
             }
-        }, $parameters);
+
+            if($type instanceof \ReflectionUnionType) {
+                throw new ContainerException(
+                  'Failed to resolve class ' . $id . ' because of union type for param ' . $name
+                );
+            }
+
+            if ($type instanceof \ReflectionNamedType && !$type->isBuiltin()) {
+                return $this->get($type->getName());
+            }
+
+            throw new ContainerException(
+                'Failed to resolve class ' . $id . ' because invalid param'
+            );
+
+        },
+            $parameters
+        );
+
+        return $reflectionClass->newInstanceArgs($dependencies);
     }
 }
